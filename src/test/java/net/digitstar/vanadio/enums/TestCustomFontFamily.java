6d9ba340a7a3d58cca6cbc7c8e9110ce2e18d877
alxsty <alxsty@8c05fa24-6bfc-5788-7cbd-f203ce01f66c>
2010-11-08 00:15:50 +0000

    massive refactoring **WORK IN PROGRESS**

---
+++ src/test/java/net/digitstar/vanadio/enums/TestCustomFontFamily.java
@@ -0,0 +1,42 @@
+/***********0********************************************************************
+ * Copyright 2010 DiGiTsTar.NeT (dTs)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ ******************************************************************************/
+
+package net.digitstar.vanadio.enums;
+
+/**
+ * Author: alx
+ * Date: 4-nov-2010
+ * <p/>
+ * Vanadio a useful pdf report generator code driven
+ */
+public class TestCustomFontFamily extends FontFamily {
+
+    public static final FontFamily FAMILYA = new TestCustomFontFamily("Font A");
+    public static final FontFamily FAMILYB = new TestCustomFontFamily("Font B");
+
+    static
+    {
+        _DEFAULT = FAMILYA;
+    }
+    private TestCustomFontFamily(String name) {
+        super(name);
+    }
+
+    public static FontFamily getDefault()
+    {
+        return TestCustomFontFamily.FAMILYA;
+    }
+}
